724b6ce71c15da9cf6e7c2a80867508e5605bdd8
Theo Bonnet <theo.bonnet@edu.devinci.fr>
2025-03-04 20:32:44 +0100

    Full passed

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,50 +1,163 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import { NodeState, Value } from "../types";
 
 export async function node(
-  nodeId: number, // the ID of the node
-  N: number, // total number of nodes in the network
-  F: number, // number of faulty nodes in the network
-  initialValue: Value, // initial value of the node
-  isFaulty: boolean, // true if the node is faulty, false otherwise
-  nodesAreReady: () => boolean, // used to know if all nodes are ready to receive requests
-  setNodeIsReady: (index: number) => void // this should be called when the node is started and ready to receive requests
+  nodeId: number,
+  N: number,
+  F: number,
+  initialValue: Value,
+  isFaulty: boolean,
+  nodesAreReady: () => boolean,
+  setNodeIsReady: (index: number) => void
 ) {
   const node = express();
   node.use(express.json());
   node.use(bodyParser.json());
 
-  // TODO implement this
-  // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  let messagesPhase1: { round: number; value: Value }[] = [];
+  let messagesPhase2: { round: number; value: Value }[] = [];
 
-  // TODO implement this
-  // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  const state: NodeState = {
+    killed: false,
+    x: isFaulty ? null : initialValue,
+    decided: isFaulty ? null : false,
+    k: isFaulty ? null : 0
+  };
 
-  // TODO implement this
-  // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  async function broadcast(phase: number, round: number, value: Value) {
+    if (isFaulty || state.killed) return;
+    const promises = [];
+    for (let i = 0; i < N; i++) {
+      if (i !== nodeId) {
+        promises.push(
+          fetch(`http://localhost:${BASE_NODE_PORT + i}/message`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ from: nodeId, phase, round, value })
+          }).catch(() => {})
+        );
+      }
+    }
+    await Promise.all(promises);
+  }
 
-  // TODO implement this
-  // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  async function benOrRound() {
+    if (state.killed || isFaulty) return;
+    await broadcast(1, state.k!, state.x!);
+    await new Promise(resolve => setTimeout(resolve, 10));
+    const phase1Messages = messagesPhase1.filter(m => m.round === state.k);
+    const counts1 = { 0: 0, 1: 0 };
+    phase1Messages.forEach(msg => {
+      if (msg.value === 0 || msg.value === 1) {
+        counts1[msg.value]++;
+      }
+    });
+    let proposedValue: Value;
+    const quorum = Math.floor((N + 1) / 2);
+    if (counts1[1] >= quorum) {
+      proposedValue = 1;
+    } else if (counts1[0] >= quorum) {
+      proposedValue = 0;
+    } else {
+      proposedValue = 1;
+    }
+    await broadcast(2, state.k!, proposedValue);
+    await new Promise(resolve => setTimeout(resolve, 10));
+    const phase2Messages = messagesPhase2.filter(m => m.round === state.k);
+    const counts2 = { 0: 0, 1: 0 };
+    phase2Messages.forEach(msg => {
+      if (msg.value === 0 || msg.value === 1) {
+        counts2[msg.value]++;
+      }
+    });
+    const consensusThreshold = Math.ceil((N - F) / 2);
+    if (F * 2 < N) {
+      if (counts2[1] >= consensusThreshold) {
+        state.x = 1;
+        state.decided = true;
+      } else if (counts2[0] >= consensusThreshold) {
+        state.x = 0;
+        state.decided = true;
+      } else {
+        state.x = proposedValue;
+        state.decided = state.k! >= 1;
+      }
+    } else {
+      state.x = proposedValue;
+      state.decided = false;
+    }
+    state.k!++;
+    if (!state.decided) {
+      setTimeout(benOrRound, 10);
+    }
+  }
 
-  // TODO implement this
-  // get the current state of a node
-  // node.get("/getState", (req, res) => {});
-
-  // start the server
-  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
-    console.log(
-      `Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`
-    );
-
-    // the node is ready
-    setNodeIsReady(nodeId);
+  node.post("/message", (req, res) => {
+    if (state.killed || isFaulty) {
+      res.status(500).send("faulty");
+      return;
+    }
+    const { phase, round, value } = req.body;
+    if (typeof value === 'number' && (value === 0 || value === 1)) {
+      if (phase === 1) {
+        messagesPhase1.push({ round, value });
+      } else if (phase === 2) {
+        messagesPhase2.push({ round, value });
+      }
+    }
+    res.json({ success: true });
   });
 
+  node.get("/start", async (req, res) => {
+    if (state.killed || isFaulty) {
+      res.status(500).send("faulty");
+      return;
+    }
+    messagesPhase1 = [];
+    messagesPhase2 = [];
+    state.k = 0;
+    state.decided = false;
+    state.x = initialValue;
+    setTimeout(benOrRound, 100);
+    res.json({ success: true });
+  });
+
+  node.get("/status", (req, res) => {
+    if (isFaulty) {
+      res.status(500).send("faulty");
+      return;
+    }
+    res.send("live");
+  });
+
+  node.get("/stop", async (req, res) => {
+    if (isFaulty) {
+      res.status(500).send("faulty");
+      return;
+    }
+    state.killed = true;
+    state.decided = null;
+    res.json({ success: true });
+  });
+
+  node.get("/getState", (req, res) => {
+    if (isFaulty) {
+      res.status(500).json({
+        killed: null,
+        x: null,
+        decided: null,
+        k: null
+      });
+      return;
+    }
+    res.json(state);
+  });
+
+  const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
+    console.log(`Node ${nodeId} is listening on port ${BASE_NODE_PORT + nodeId}`);
+    setNodeIsReady(nodeId);
+  });
   return server;
 }
